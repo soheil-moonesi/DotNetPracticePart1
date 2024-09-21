8b9de4a9ad9ce51d0744fa80ce048522858a2c5f
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2024-09-21 09:54:49 +0330

    Add project files.

---
+++ IEnumerable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+
+
+public class TestCollection : IEnumerable
+{
+    public string[] Course = { "Math", "Science", "Philosophy" };
+
+    // Required method for implementing IEnumerable
+    // it is use for find and return next element in collection
+    public IEnumerator GetEnumerator()
+    {
+        foreach (var c in Course)
+        {
+            yield return c;
+        }
+    }
+}
+
