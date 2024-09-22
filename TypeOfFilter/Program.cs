08b93a11e19732a6b16398b818cde7cfb2558fe4
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2024-09-22 21:52:56 +0330

    Add : typeOf filter

---
+++ TypeOfFilter/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //IList can have multiple member with different types
+        IList list = new ArrayList();
+        list.Add(4);
+        list.Add("s");
+        list.Add(new Person() { Id = 1, Name = "soheil" });
+        list.Add(20);
+
+        var IntResult = list.OfType<int>();
+
+        foreach (var c in IntResult)
+        {
+            Console.WriteLine(c.ToString());
+        }
+
+
+
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
