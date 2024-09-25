513c2c990448b26043b80653762dd9e53f58b33f
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2024-09-25 16:20:45 +0330

    Add: Heart Talk Project

---
+++ MVCSee/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCSee.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SortingFilter/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Person> Persons = new List<Person>()
+        {
+            new Person() { Id = 1, Name = "soheil", Age = 33 },
+            new Person() { Id = 2, Name = "sepehr", Age = 28 },
+            new Person() { Id = 3, Name = "sara", Age = 25 },
+            new Person() { Id = 4, Name = "samin", Age = 19 },
+            new Person() { Id = 5, Name = "sina", Age = 50 }
+        };
+
+        var dataOrderBy = Persons.OrderBy(p => p.Age);
+
+        foreach (var p in dataOrderBy)
+        {
+            Console.WriteLine(p.Age);
+        }
+
+
+
+
+    }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
