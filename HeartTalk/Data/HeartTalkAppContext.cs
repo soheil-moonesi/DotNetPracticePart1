8673ce9370e37c6f1f6ede3f2aaae0b54d4cd512
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2024-09-25 16:48:34 +0330

    Add : OnConfiguring database

---
+++ HeartTalk/Data/HeartTalkAppContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace HeartTalk.Data
+{
+    public class HeartTalkAppContext : DbContext
+    {
+     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+     {
+         optionsBuilder.UseSqlServer("Data Source =.; Initial Catalog = HeartTalkDb; Integrated Security = True; TrustServerCertificate = True");
+     }
+    }
+}
