c9f28ef452e83420bc7f3b11e955007c581b50a3
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2024-09-25 17:05:35 +0330

    Add : Note class and DbSet

--- HeartTalk/Data/HeartTalkAppContext.cs
+++ HeartTalk/Data/HeartTalkAppContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HeartTalk.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace HeartTalk.Data
 {
     public class HeartTalkAppContext : DbContext
-    {
+    { 
+        public DbSet<Note> Notes { get; set; }
      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
      {
          optionsBuilder.UseSqlServer("Data Source =.; Initial Catalog = HeartTalkDb; Integrated Security = True; TrustServerCertificate = True");
--- HeartTalk/Models/Note.cs
+++ HeartTalk/Models/Note.cs
@@ -2,5 +2,10 @@
 {
     public class Note
     {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime DatePosted { get; set; }
+        public int SympathyCount { get; set; }
     }
+
 }
