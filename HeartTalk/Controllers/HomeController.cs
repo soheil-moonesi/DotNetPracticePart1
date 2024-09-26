4e8a24e846ecc40366b78840568cc395302aa815
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2024-09-26 10:48:02 +0330

    Add : view for notes

--- HeartTalk/Controllers/HomeController.cs
+++ HeartTalk/Controllers/HomeController.cs
@@ -1,11 +1,15 @@
 using HeartTalk.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using HeartTalk.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace HeartTalk.Controllers
 {
     public class HomeController : Controller
     {
+        public HeartTalkAppContext DatabaseContext = new HeartTalkAppContext();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -13,10 +17,16 @@ namespace HeartTalk.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await DatabaseContext.Notes.ToListAsync());
         }
+
 
         public IActionResult Privacy()
         {
