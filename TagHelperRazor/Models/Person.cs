7225cdf4079ed0d22e5152153fbedf5a895968af
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2024-10-03 17:11:48 +0330

    Add: New Project ,Show TextBox with lable with Razor

---
+++ TagHelperRazor/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Cors;
+
+namespace TagHelperRazor.Models
+{
+    public class Person
+    {
+        public int id { get; set; }
+
+        [DisplayName("نام شخص")]
+        public string Name { get; set; }
+    }
+}
