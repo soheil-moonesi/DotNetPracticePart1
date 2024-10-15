2400d52e39dec59df3e7f425a8798f139b625ead
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2024-10-15 21:29:42 +0330

    Add: Comment and Change Note structure

---
+++ HeartTalk/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace HeartTalk.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string CommentText { get; set; }
+        
+        public int NoteId { get; set; }
+
+        public Note Note { get; set; }
+
+    }
+}
--- HeartTalk/Models/Note.cs
+++ HeartTalk/Models/Note.cs
@@ -6,6 +6,7 @@
         public string Content { get; set; }
         public DateTime DatePosted { get; set; }
         public int SympathyCount { get; set; } = 0;
+        public ICollection<Comment> Comments { get; set; }
     }
 }
 
