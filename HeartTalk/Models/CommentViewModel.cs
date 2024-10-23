dbe6eeba7821a1d7ccab2c1503781534e1288e3b
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2024-10-23 09:24:59 +0330

    Add: Comment to View

---
+++ HeartTalk/Models/CommentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HeartTalk.Models
+{
+    public class CommentViewModel
+    {
+           public Comment? Comment { get; set; }
+        public IEnumerable<Comment>? Comments { get; set; }
+
+    }
+}
